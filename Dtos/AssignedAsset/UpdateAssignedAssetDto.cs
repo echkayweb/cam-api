e64cccf63f2e3edfa4a5cc6bad72d7af6b055944
echkayweb <hkxxx843@gmail.com>
2022-08-04 13:14:18 +0530

    Updating updateassignedassetdto

--- Dtos/AssignedAsset/UpdateAssignedAssetDto.cs
+++ Dtos/AssignedAsset/UpdateAssignedAssetDto.cs
@@ -3,8 +3,8 @@ namespace cam_api.Dtos.AssignedAsset
     public class UpdateAssignedAssetDto
     {
         public int AssignedAssetId { get; set; }
-        public int? AssetId { get; set; }
-        public int? EmployeeId { get; set; }
+        // public int? AssetId { get; set; }
+        // public int? EmployeeId { get; set; }
         public string? Remarks { get; set; } = string.Empty;
         public DateTime DateAssigned { get; set; }
         public DateTime? DateReturned { get; set; }
